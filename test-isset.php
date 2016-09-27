a06c44e852ec1a563500035747d70540a305a3eb
David Foltyn <david.foltyn@peckadesign.cz>
2016-09-27 22:45:04 +0200

    update

--- test-isset.php
+++ test-isset.php
@@ -27,17 +27,43 @@ class TestClass implements ArrayAccess {
 
 $obj = new TestClass;
 
-echo "* Calling isset(\$obj['foo'])...";
+echo '<pre>
+protected $container = [];
+
+public function offsetExists($index) {
+	echo "offsetExists(" . var_export($index, true) . ")\n";
+	return isset($this->container[$index]);
+}
+
+public function offsetGet($index) {
+	echo "offsetGet(" . var_export($index, true) . ")\n";
+	return $this->container[$index];
+}
+
+public function offsetSet($index, $value) {
+	echo "offsetSet(". var_export($index, true) . ", " . var_export($value, true) . ")\n";
+	$this->container[$index] = $value;
+}
+
+public function offsetUnset($index) {
+	echo "offsetUnset(" . var_export($index, true) . ")\n";
+	unset($this->container[$index]);
+}
+</pre>';
+
+echo '<hr>';
+
+echo "calling isset(\$obj['foo'])...";
 var_dump(isset($obj['foo']));
 echo "<br><br>";
 
-echo "* Calling isset(\$obj['foo']['bar'])...\n";
+echo "calling isset(\$obj['foo']['bar'])...\n";
 var_dump(isset($obj['foo']['bar']));
 echo "<br><br>";
 
-echo "* Setting offset...\n";
+echo "setting offset...\n";
 $obj['foo'] = null;
 echo "<br><br>";
 
-echo "* Calling isset(\$obj['foo']['bar'])...\n";
+echo "calling isset(\$obj['foo']['bar'])...\n";
 var_dump(isset($obj['foo']['bar']));
