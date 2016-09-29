f27e30969c462b67afb82c411fb2f4e48e6f7efe
David Foltýn <david.foltyn@peckadesign.cz>
2016-09-29 08:10:07 +0200

    update

--- test-isset.php
+++ test-isset.php
@@ -26,58 +26,6 @@ class TestClass implements ArrayAccess {
 
 }
 
-class PropertyTest
-{
-    
-    private $data = array();
-
-	public $declared = 1;
-    
-    private $hidden = 2;
-
-
-    /** SET **/
-    public function __set($name, $value)
-    {
-        echo "Setting '$name' to '$value'\n";
-        $this->data[$name] = $value;
-    }
-
-    /** GET **/
-    public function __get($name)
-    {
-        echo "Getting '$name'\n";
-        if (array_key_exists($name, $this->data)) {
-            return $this->data[$name];
-        }
-
-        $trace = debug_backtrace();
-        trigger_error(
-            'Undefined property via __get(): ' . $name .
-            ' in ' . $trace[0]['file'] .
-            ' on line ' . $trace[0]['line'],
-            E_USER_WARNING);
-        return null;
-    }
-
-    public function __isset($name)
-    {
-        echo "Is '$name' set?\n";
-        return isset($this->data[$name]);
-    }
-
-    public function __unset($name)
-    {
-        echo "Unsetting '$name'\n";
-        unset($this->data[$name]);
-    }
-
-    public function getHidden()
-    {
-        return $this->hidden;
-    }
-}
-
 
 /**
 * simple test
@@ -103,26 +51,4 @@ echo "\n\n";
 echo "calling isset(\$obj['foo']['bar'])...\n";
 var_dump(isset($obj['foo']['bar']));
 
-echo "</pre>";
-echo "<hr>";
-
-
-
-echo "<pre>";
-$obj = new PropertyTest;
-
-$obj->a = 1;
-echo $obj->a . "\n\n";
-
-var_dump(isset($obj->a));
-echo "\n\n";
-
-unset($obj->a);
-var_dump(isset($obj->a));
-echo "\n\n";
-
-echo $obj->declared . "\n\n";
-
-echo $obj->getHidden() . "\n";
-echo $obj->hidden . "\n";
 echo "</pre>";
